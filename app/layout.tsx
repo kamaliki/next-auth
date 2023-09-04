fa88e742d7f4c4e1c17d3c1e510de2fd77f09a14
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2023-09-04 21:02:52 +0300

    first phase

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <AuthProvider> */}
+        <AuthProvider>
           <Navbar />
           {children}
-        {/* </AuthProvider> */}
+        </AuthProvider>
       </body>
     </html>
   );
